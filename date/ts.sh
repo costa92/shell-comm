b8559ad90748503d6a959436b18186a8fe1393c0
costa <costa@hellotalk.com>
2023-10-25 17:58:35 +0800

    fix: 修改时间转换

--- date/ts.sh
+++ date/ts.sh
@@ -3,12 +3,17 @@
 # 判断变量是否为数字
 if [[ "$1" =~ ^[0-9]+$ ]];then
   # 将时间戳转换为日期时间格式
-  date=$(date -d @$1 +"%Y-%m-%d %H:%M:%S")
-  echo "$date"  
+  date_format="%Y-%m-%d %H:%M:%S"
+  date_str=$(date -d @$1 +"$date_format")
+  echo "$date_str"  
 fi
 
 # 判断是否我时间格式
-if [ -n "$1" ] && date -d "$1" > /dev/null 2>&1; then
+
+# 定义时间格式的正则表达式
+time_regex="^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$"
+day_regex="^[0-9]{4}-[0-9]{2}-[0-9]{2}$"
+if [[ "$1" =~ $time_regex || "$1" =~ $day_regex  ]]; then
   date=$(date -d "$1" +'%s')
   echo "$date"
 fi
---
+++ k8s-plug/krew.sh
@@ -0,0 +1,5 @@
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.2/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  ./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" install \
+    --manifest=krew.yaml --archive=krew.tar.gz
